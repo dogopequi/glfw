b4e29fa8e59a3479e6b7f2cc8493dda87129aec7
dogo <68162119+dogopequi@users.noreply.github.com>
2025-05-21 21:14:58 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputDir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
@@ -8,24 +9,28 @@ project "GLFW"
     files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
+
+        -- Core source
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
         "src/window.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c",
         "src/platform.c",
+        "src/internal.h",
+        "src/osmesa_context.c",
+        "src/egl_context.c",
         "src/null_init.c",
-        "src/internal.h"
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
+        "src/null_joystick.h",
+        "src/null_platform.h"
     }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
-        staticruntime "On"
 
         files {
             "src/win32_init.c",
@@ -35,7 +40,11 @@ project "GLFW"
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
-            "src/win32_module.c"
+            "src/win32_module.c",
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+            "src/win32_thread.h",
+            "src/win32_time.h"
         }
 
         defines {
@@ -43,23 +52,18 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
-
     filter "system:linux"
         pic "On"
         systemversion "latest"
-        staticruntime "On"
-        buildoptions { "-std=c11" }
 
         files {
-            -- X11 platform
+            -- X11
             "src/x11_init.c",
             "src/x11_monitor.c",
             "src/x11_window.c",
             "src/x11_platform.h",
 
-            -- Wayland platform
+            -- Wayland
             "src/wl_init.c",
             "src/wl_monitor.c",
             "src/wl_window.c",
@@ -69,11 +73,11 @@ project "GLFW"
             "src/xkb_unicode.c",
             "src/xkb_unicode.h",
 
-            -- Linux joystick support
+            -- Linux input
             "src/linux_joystick.c",
             "src/linux_joystick.h",
 
-            -- POSIX layer
+            -- POSIX platform
             "src/posix_module.c",
             "src/posix_poll.c",
             "src/posix_poll.h",
@@ -82,15 +86,7 @@ project "GLFW"
             "src/posix_time.c",
             "src/posix_time.h",
 
-            -- Null platform
-            "src/null_init.c",
-            "src/null_monitor.c",
-            "src/null_window.c",
-            "src/null_joystick.c",
-            "src/null_joystick.h",
-            "src/null_platform.h",
-
-            -- GLX context
+            -- GLX
             "src/glx_context.c"
         }
 
